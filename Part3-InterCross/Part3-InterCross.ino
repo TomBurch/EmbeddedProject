cc667362bda57969d260ac4535b10176aef9a859
TomBurch <thomas.burch1306@gmail.com>
2020-01-12 18:11:23 +0000

    Added InterCross

---
+++ Part3-InterCross/Part3-InterCross.ino
@@ -0,0 +1,56 @@
+//#include <TrafficLight.h>
+#include <PedLight.h>
+#include <Intersection.h>
+
+TrafficLight light1;
+TrafficLight light2;
+Intersection intersect;
+PedLight pedLight;
+
+char command;
+
+void setup() {
+  //Enable serial input
+  Serial.begin(9600);
+  
+  //TLight1 pins
+  pinMode(13, OUTPUT); //Red
+  pinMode(12, OUTPUT); //Yellow
+  pinMode(11, OUTPUT); //Green
+  
+  //TLight2 pins
+  pinMode(10, OUTPUT); //Red
+  pinMode(9, OUTPUT); //Yellow
+  pinMode(8, OUTPUT); //Green
+
+  //PedLight pins
+  pinMode(6, OUTPUT); //Red
+  pinMode(5, OUTPUT); //Green
+  
+  //Setup objects
+  light1.setup(13, 12, 11, "TLight1");
+  light2.setup(10, 9, 8, "TLight2");
+  pedLight.setup(6, 5, "PedLight");
+  intersect.setup(light1, light2);
+
+  //Set PedLight to red
+  pedLight.setLights(0);
+}
+
+void loop() {
+  //Keep changing intersection until input
+  while (!Serial.available()) {
+    intersect.change(false);
+    delay(1000);
+    intersect.change(true);
+    delay(1000);
+  }
+  command = Serial.read();
+  if (command == 'c') {
+    pedLight.setTraffic(true); //Red->Green
+    delay(2000);
+    pedLight.setTraffic(false); //Green->Red
+    delay(2000);
+  }
+  command = '\0';
+}
---
+++ Part4-Sort/Part4-Sort.ino
@@ -0,0 +1,66 @@
+#include <NumberDisplay.h>
+#include <Quicksort.h>
+
+//Generate 50 random ints(0-255) and place them into randArray[]
+void generateArray(int randArray[]) {
+  for (int i = 0; i < 50; i++) {
+    int randInt = random(0, 256);
+    randArray[i] = randInt;
+  }
+}
+
+//Loop through array[] and print each int
+void outArray(int array[]) {
+  for(int i = 0; i < 50; i++) {
+    Serial.print(array[i]);
+    Serial.print(", ");
+  }
+  Serial.print("\n");
+}
+
+int randArray[50];
+char command;
+NumberDisplay display;
+
+void setup() {
+  //Enable serial input
+  Serial.begin(9600);
+  
+  //Create random seed
+  randomSeed(analogRead(0));
+  
+  //Display pins
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  display.setup(5, 6, 8, 9, 10, 11, 12, 13);
+}
+
+void loop() {
+  while(!Serial.available()) {}
+  command = Serial.read();
+  if (command == 'c') {
+    generateArray(randArray);
+  
+    outArray(randArray);
+    quicksort(randArray, 0, 49);
+    outArray(randArray);
+    
+    Serial.println(randArray[0]);
+    Serial.println(randArray[49]);
+  
+    display.display(randArray[0]);
+    delay(3000);
+    display.display(randArray[49]);
+    delay(3000);
+	
+    Serial.println();
+  }
+  command = '\0';
+}
